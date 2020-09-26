c9bc685fc6e0336a74b86fa95b379a136c3553eb
Costas Basdekis <costas.basdekis@gmail.com>
2020-09-26 22:45:53 +0100

    Fix indentation type

--- octoprint_marlingcodedocumentation/__init__.py
+++ octoprint_marlingcodedocumentation/__init__.py
@@ -5,39 +5,39 @@ import octoprint.plugin
 
 
 class MarlingcodedocumentationPlugin(
-	octoprint.plugin.StartupPlugin, octoprint.plugin.SettingsPlugin,
-	octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin):
+    octoprint.plugin.StartupPlugin, octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin):
 
-	def on_after_startup(self):
-		self._logger.info("Hello MarlingcodedocumentationPlugin!")
+    def on_after_startup(self):
+        self._logger.info("Hello MarlingcodedocumentationPlugin!")
 
-	def get_settings_defaults(self):
-		return {
-		}
+    def get_settings_defaults(self):
+        return {
+        }
 
-	def get_assets(self):
-		return dict(
-			js=["js/all_codes.js", "js/marlingcodedocumentation.js"],
-			css=["css/marlingcodedocumentation.css"],
-			less=["less/marlingcodedocumentation.less"]
-		)
+    def get_assets(self):
+        return dict(
+            js=["js/all_codes.js", "js/marlingcodedocumentation.js"],
+            css=["css/marlingcodedocumentation.css"],
+            less=["less/marlingcodedocumentation.less"]
+        )
 
-	def get_update_information(self):
-		return dict(
-			marlingcodedocumentation=dict(
-				displayName="Marlingcodedocumentation Plugin",
-				displayVersion=self._plugin_version,
+    def get_update_information(self):
+        return dict(
+            marlingcodedocumentation=dict(
+                displayName="Marlingcodedocumentation Plugin",
+                displayVersion=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="costas-basdekis",
-				repo="MarlinGcodeDocumentation",
-				current=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="costas-basdekis",
+                repo="MarlinGcodeDocumentation",
+                current=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/costas-basdekis/MarlinGcodeDocumentation/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/costas-basdekis/MarlinGcodeDocumentation/archive/{target_version}.zip"
+            )
+        )
 
 
 __plugin_name__ = "Marlin GCode Documentation"
@@ -46,11 +46,11 @@ __plugin_pythoncompat__ = ">=3,<4"
 
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = MarlingcodedocumentationPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = MarlingcodedocumentationPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config":
-		__plugin_implementation__.get_update_information,
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config":
+        __plugin_implementation__.get_update_information,
+    }
