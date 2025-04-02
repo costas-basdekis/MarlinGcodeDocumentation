376b772d467929f6fed49984bcd77d20d8a70fa3
Costas Basdekis <costas.basdekis@gmail.com>
2025-04-02 10:30:43 +0100

    Bump version to 0.20.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_marlingcodedocumentation"
 plugin_name = "Marlin GCode Documentation"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.19.0"
+plugin_version = "0.20.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
